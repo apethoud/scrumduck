13f63e1510c94ea05fce473acdaaa11129b79165
Andrew Pethoud <andrewpethoud@gmail.com>
2023-02-24 17:06:00 -0800

    chore(exampleData): add example sprint data

---
+++ client/src/api/api.ts
@@ -0,0 +1,30 @@
+const exampleSprintData = {
+  id: 1,
+  name: "12",
+  projectId: 1,
+  userStories: [
+    {
+      id: 1,
+      title: "Calendar Feature",
+      storyText: "As a user, I want to select a date for my event so I can keep track of scheduling.",
+      githubIssueNumber: 24,
+      githubLinkUrl: "http://github.com"
+    },
+    {
+      id: 2,
+      title: "Profile Feature",
+      storyText: "As a user, I want to add personal details about myself so other users can identify me.",
+      githubIssueNumber: 27,
+      githubLinkUrl: "http://github.com"
+    },
+    {
+      id: 1,
+      title: "Login Feature",
+      storyText: "As a user, I want to access my account securely to avoid data security issues.",
+      githubIssueNumber: 5,
+      githubLinkUrl: "http://github.com"
+    },
+  ]
+}
+
+export default exampleSprintData;
