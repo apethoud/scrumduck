a9897cb4bdb72a7c7b3d773591ac25c6255e27a4
Andrew Pethoud <andrewpethoud@gmail.com>
2023-03-04 13:18:06 -0800

    feat(sprint-view): add dropdown icon to sprint selector

--- client/src/components/SprintSelector.tsx
+++ client/src/components/SprintSelector.tsx
@@ -1,7 +1,12 @@
 import { SecondaryHeader } from "./styledComponents/common";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 
 export default function SprintSelector({ selectedSprintName }) {
   return (
-    <SecondaryHeader>Sprint { selectedSprintName }</SecondaryHeader>
+    <div style={{ display: "flex", alignItems: "center", marginBottom: 24 }}>
+      <SecondaryHeader style={{ marginRight: 8 }}>Sprint { selectedSprintName }</SecondaryHeader>
+      <FontAwesomeIcon icon={faAngleDown} />
+    </div>
   )
 }
