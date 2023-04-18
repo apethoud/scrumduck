d2be757df4dc4d348e0e04749c8c70af4294b96c
Andrew Pethoud <andrewpethoud@gmail.com>
2023-04-17 20:03:52 -0700

    update to createBrowserRouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
 import {
-  BrowserRouter,
-  Routes,
-  Route
-} from 'react-router-dom';
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import SiteHeader from './components/SiteHeader';
 import MarketingPage from './components/MarketingPage';
 import Login from './components/Login';
@@ -13,21 +12,41 @@ import AppContainer from './components/AppContainer';
 import NotFound from './components/NotFound';
 import StoryView from './components/StoryView';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SiteHeader />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <MarketingPage />
+      },
+      {
+        path: "login",
+        element: <Login />
+      },
+      {
+        path: "app",
+        element: <AppContainer />,
+        children: [
+          {
+            path: "sprint/:sprintId",
+            element: <SprintView />
+          },
+          {
+            path: "story/:storyId",
+            element: <StoryView />
+          }
+        ]
+      }
+    ]
+  },
+]);
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SiteHeader />}>
-          <Route index element={<MarketingPage />} />
-          <Route path="login" element={<Login />} />
-          <Route path="app" element={<AppContainer />}>
-            <Route path="sprint/:sprintId" element={<SprintView />} />
-            <Route path="story/:storyId" element={<StoryView />} />
-          </Route>
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   );
 }
 
