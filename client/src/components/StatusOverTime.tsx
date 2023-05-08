bebf4ed7dc7d587c14dfa6ad9f7761467e6178d3
Andrew Pethoud <andrewpethoud@gmail.com>
2023-05-07 18:02:55 -0700

    feat(SprintView): add sprint start and end dates to StatusOverTime WIP

--- client/src/components/StatusOverTime.tsx
+++ client/src/components/StatusOverTime.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import { Flex } from './styledComponents/common';
+import first from 'lodash/first';
+import last from 'lodash/last';
 
 export default function StatusOverTime({ days }) {
   const DayBox = styled.div`
@@ -18,20 +20,34 @@ export default function StatusOverTime({ days }) {
     };
   `
 
+  const StartDate = styled.div`
+    border-left: 1px solid black;
+  `
+
+  const EndDate = styled.div`
+    border-right: 1px solid black;
+  `
+
   console.log("days is: ", days);
 
   return (
-    <Flex>
-      {days.map(day => (
-        <DayBox>
-          {day.statuses.map(increment => (
-            <StatusSegment 
-              percentage={increment.percentage}
-              color={increment.status ? increment.status.color : "#ebebeb"}
-            />
-          ))}
-        </DayBox>
-      ))}
-    </Flex>
+    <div>
+      <Flex>
+        {days.map(day => (
+          <DayBox>
+            {day.statuses.map(increment => (
+              <StatusSegment 
+                percentage={increment.percentage}
+                color={increment.status ? increment.status.color : "#ebebeb"}
+              />
+            ))}
+          </DayBox>
+        ))}
+      </Flex>
+      <Flex style={{ justifyContent: "space-between", marginTop: 8 }}>
+        <StartDate>{first(days).date}</StartDate>
+        <EndDate>{last(days).date}</EndDate>
+      </Flex>
+    </div>
   )
 }
